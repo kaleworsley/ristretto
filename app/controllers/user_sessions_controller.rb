da77eed0165771fcae4a8ad907064589cd4e190a
Kale Worsley <kale@egressive.com>
2010-09-02 03:12:17 +1200

    Refactored login redirect.

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,11 +2,6 @@ class UserSessionsController < ApplicationController
   skip_before_filter :require_login, :only => [:new, :create]
 
   def new
-    unless params[:redirect].blank?
-      @redirect = params[:redirect]
-    else
-      @redirect = root_url
-    end
     @user_session = UserSession.new
   end
 
@@ -14,7 +9,7 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = 'Logged in.'
-      redirect_to params[:redirect] || root_url
+      redirect_back_or_default root_url
     else
       render :action => 'new'
     end
@@ -24,10 +19,6 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.find
     @user_session.destroy
     flash[:notice] = 'Successfully logged out.'
-    unless params[:redirect].blank?
-      redirect_to params[:redirect]
-    else
-      redirect_to root_url
-    end
+      redirect_back_or_default root_url
   end
 end
