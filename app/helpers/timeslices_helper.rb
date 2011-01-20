ecd3ed113aa10a416961aa5e1485a569dc882c66
Kale Worsley <kale@egressive.com>
2011-01-21 10:00:02 +1300

    Adds filtering to queries to only return current/proposed projects and rejected/not started/started/delivered tasks and sorts them by name.

--- app/helpers/timeslices_helper.rb
+++ app/helpers/timeslices_helper.rb
@@ -1,17 +1,17 @@
 module TimeslicesHelper
   def mcdropdown_ul(user)
     out = '<ul id="task_mc_dropdown" class="mcdropdown_menu">'
-    user.current_customers.each do |customer|
+    user.current_customers.sort_by(&:name).each do |customer|
       out += '<li rel="c' + customer.id.to_s + '">'
       out += customer.name
-      if customer.projects.present?
+      if customer.projects.find(:all, :conditions => {:state => ['proposed', 'current']}, :order => :name).present?
         out += '<ul>'
-        customer.projects.each do |project|
+        customer.projects.find(:all, :conditions => {:state => ['proposed', 'current']}, :order => :name).each do |project|
           out += '<li rel="p' + project.id.to_s + '">'
           out += project.name
-          if project.tasks.present?
+          if project.tasks.find(:all, :conditions => {:state => Task.stategroups[:current]}, :order => :name).present?
             out += '<ul>'
-            project.tasks.each do |task|
+            project.tasks.find(:all, :conditions => {:state => Task.stategroups[:current]}, :order => :name).each do |task|
               out += '<li rel="' + task.id.to_s + '">'
               out += task.name
               out += '</li>'
